aa7451dad42974614868938f35a8916439992fea
Maisam Mohammadi <riomaisam@gmail.com>
2026-05-17 14:06:11 +0200

    Build problem is solved
    Now I can build the game and run it.

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Codice.Client.Common.EventTracking.TrackFeatureUseEvent.Features.DesktopGUI.Filters;
 
 public class Skeleton : MonoBehaviour
 {
